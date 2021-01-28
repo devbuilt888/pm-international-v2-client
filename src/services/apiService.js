3c22f5665d5292a0644dac9b3a845d17ceeadcd1
laptopcerveza <joseperezprogramming@outlook.com>
2021-01-28 16:52:16 -0500

    Added correct email address to ui translation sheet

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -10,6 +10,7 @@ const GET_USER_URL = id => `https://megalodon-pm-api.herokuapp.com/api/v1/users/
 
 
 export const getUser = (id) => axios.get(GET_USER_URL(id))
+// export const update = 
 export const createUser = (data) => axios.post(SIGNUP_URL, data)
 export const getToken = (amount, url) => axios.get(TOKEN_URL, { params: { amount, url } })
-export const createOrder = (data) => axios.post(ORDER_URL, data)
+export const createOrder = (data) => axios.post(ORDER_URL, data) 
